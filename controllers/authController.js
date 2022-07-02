9a16719f8c67dc9dec7c9a9d9a30472e3000a038
StephaneCash <kikonistephane@gmail.com>
2022-07-02 12:48:02 +0100

    createUser

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+const userModel = require('../models/userModel');
+
+const signUp = async (req, res) => {
+    const { pseudo, email, password } = req.body;
+
+    try {
+        const user = await userModel.create({ pseudo, email, password });
+        res.status(201).json({ user: user.id });
+    }
+    catch (err) {
+        res.status(500).json({ err: err });
+    }
+}
+
+module.exports = {
+    signUp
+}
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config({ path: './config/.env' })
 require('./config/db')
 const app = express();
+
+
+//routes
+app.use('/api/users', userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`App écoute sur le port ${process.env.PORT}`)
