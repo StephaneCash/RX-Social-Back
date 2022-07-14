9e3aa692551d89ccb790a8e1b5bb0ffaee3935ff
StephaneCash <kikonistephane@gmail.com>
2022-07-14 13:22:47 +0100

    controller post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,6 @@
 const postModel = require('../models/postModel');
 const userModel = require('../models/userModel');
+const ObjetID = require('mongoose').Types.ObjectId;
 
 const readPost = (req, res) => {
 
