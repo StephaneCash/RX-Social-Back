3db385e54227d53f6da5dc3f230e024b10b74012
StephaneCash <kikonistephane@gmail.com>
2022-07-14 13:18:23 +0100

    controllerpost

---
+++ controllers/postController.js
@@ -0,0 +1,24 @@
+const postModel = require('../models/postModel');
+
+const readPost = (req, res) => {
+
+}
+
+const createPost = (req, res) => {
+
+}
+
+const updatePost = (req, res) => {
+
+}
+
+const deletePost = (req, res) => {
+
+}
+
+module.exports = {
+    readPost,
+    createPost,
+    updatePost,
+    deletePost
+}
