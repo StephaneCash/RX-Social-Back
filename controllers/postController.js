f0f51ad899bcffaaa755d3ae4399049531745734
StephaneCash <kikonistephane@gmail.com>
2022-07-18 14:37:12 +0100

    gg

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,11 +3,27 @@ const userModel = require('../models/userModel');
 const ObjetID = require('mongoose').Types.ObjectId;
 
 const readPost = (req, res) => {
-
+    postModel.find((err, docs) => {
+        if (!err) res.send(docs);
+        else console.log('Erreur to get data : ', +err)
+    })
 }
 
 const createPost = (req, res) => {
+    const newPost = new postModel({
+        posterId: req.body.posterId,
+        message: req.body.message,
+        video: req.body.video,
+        likers: [],
+        comments: [],
+    });
 
+    try {
+        const post = newPost.save();
+        return res.status(201).json(post)
+    } catch (err) {
+        return res.status(400).json(err)
+    }
 }
 
 const updatePost = (req, res) => {
